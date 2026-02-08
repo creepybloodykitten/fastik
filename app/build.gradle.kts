c734533e67e829b4377068a08fcf350a6a4d0619
nktdrgvl <nikita2005420054@gmail.com>
2026-02-08 17:23:03 +0700

    searchbar: rewrite ui and make it tiny

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.ui.graphics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
